3e19db6b9049bad8da286e2c6436e505b48384f8
Sasha Goloshchapov <ferrata@hey.com>
2025-01-24 18:03:23 -0600

    feat: ActionNode can calculate times

--- components/ActionGraph/ActionGraph.tsx
+++ components/ActionGraph/ActionGraph.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 
+import { layout } from '@dagrejs/dagre'
+import { Graph } from '@dagrejs/graphlib'
 import {
   Background,
   ConnectionLineType,
@@ -15,7 +17,6 @@ import {
   useNodesState,
 } from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
-import dagre from 'dagre'
 
 import { ActionNode, NodeData } from './ActionNode'
 
@@ -60,8 +61,8 @@ function toNode(props: NodeProps): Node {
 const nodeWidth = 200
 const nodeHeight = 50
 
-function createGraph(
-  dagreGraph: dagre.graphlib.Graph,
+function calculateGraph(
+  dagreGraph: Graph,
   args: {
     orientation: string
     nodes: NodeProps[]
@@ -78,7 +79,13 @@ function createGraph(
   })
 
   const edges: Edge[] = args.edges
-    .filter(edge => edge.fromNodeID && edge.toNodeID)
+    .filter(
+      edge =>
+        edge.fromNodeID &&
+        edge.toNodeID &&
+        dagreGraph.node(edge.fromNodeID) &&
+        dagreGraph.node(edge.toNodeID)
+    )
     .map(edge => ({
       id: `edge-${edge.fromNodeID}-${edge.toNodeID}`,
       source: edge.fromNodeID,
@@ -90,27 +97,54 @@ function createGraph(
     dagreGraph.setEdge(edge.source, edge.target)
   })
 
+  function getParentsTiming(nodeID: string): number {
+    const node = args.nodes.find(node => node.id === nodeID)
+    if (node == null) return 0
+
+    const parents = dagreGraph.predecessors(nodeID)
+    if (parents == null || parents.length === 0) {
+      return node.time
+    }
+
+    const parentTimings = parents.map(parent => getParentsTiming(parent))
+    const maxParentTiming = Math.max(...parentTimings)
+    return node.isCalculated ? maxParentTiming : maxParentTiming + node.time
+  }
+
+  const calculatedTiming = dagreGraph.nodes().reduce((acc, nodeID) => {
+    const node = args.nodes.find(node => node.id === nodeID)
+    if (node == null || !node.isCalculated) return acc
+
+    const timing = getParentsTiming(nodeID)
+    acc.set(nodeID, timing)
+    return acc
+  }, new Map<string, number>())
+
   const isHorizontal = args.orientation === 'LR'
   dagreGraph.setGraph({ rankdir: args.orientation })
-  dagre.layout(dagreGraph)
 
-  const newNodes: Node[] = initialNodes.map(node => {
+  // @ts-ignore: types mismatch
+  layout(dagreGraph)
+
+  const calculatedNodes: Node[] = initialNodes.map(node => {
     const nodeWithPosition = dagreGraph.node(node.id)
+    const timing = calculatedTiming.get(node.id) ?? node.data.time ?? 0
     const newNode = {
       ...node,
       targetPosition: isHorizontal ? Position.Left : Position.Top,
       sourcePosition: isHorizontal ? Position.Right : Position.Bottom,
       position: { x: nodeWithPosition.x, y: nodeWithPosition.y },
+      data: { ...node.data, time: timing },
     }
 
     return newNode
   })
 
-  return { nodes: newNodes, edges }
+  return { nodes: calculatedNodes, edges }
 }
 
 export function ActionGraph({ className, graph, ...rest }: Props) {
-  const dagreGraph = useRef(new dagre.graphlib.Graph())
+  const dagreGraph = useRef(new Graph())
   dagreGraph.current.setDefaultEdgeLabel(() => ({}))
 
   const initialNodes = useMemo(() => rest.nodes.filter(node => node.id), [rest.nodes])
@@ -123,11 +157,14 @@ export function ActionGraph({ className, graph, ...rest }: Props) {
   const [edges, setEdges, __] = useEdgesState<Edge>([])
 
   useEffect(() => {
-    const { nodes: recalculatedNodes, edges: recalculatedEdges } = createGraph(dagreGraph.current, {
-      orientation: graph.orientation,
-      nodes: initialNodes,
-      edges: initialEdges,
-    })
+    const { nodes: recalculatedNodes, edges: recalculatedEdges } = calculateGraph(
+      dagreGraph.current,
+      {
+        orientation: graph.orientation,
+        nodes: initialNodes,
+        edges: initialEdges,
+      }
+    )
 
     setNodes(recalculatedNodes)
     setEdges(recalculatedEdges)
